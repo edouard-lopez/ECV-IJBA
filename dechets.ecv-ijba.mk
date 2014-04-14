55234f553c35b1cd397fab25f308a4b2ad718cac
Edouard Lopez <dev+git@edouard-lopez.com>
2014-04-15 00:56:58 +0200

    refactor(mk): place temporary files in tmp/

--- dechets.ecv-ijba.mk
+++ dechets.ecv-ijba.mk
@@ -26,24 +26,29 @@ contours-gironde=contours-gironde-EPCI
 get-contours-france: france-epci-100m-shp.zip
 get-contours-gironde: gironde-odt_epci2014-shp.zip
 
+get-contours-france: tmp/france-epci-100m-shp.zip
+get-contours-gironde: tmp/gironde-odt_epci2014-shp.zip
+convert2geojson: tmp/*/gironde-epci.json
 # @alias: get-contours-france
 # EPCI de Gironde
 # @source: http://catalogue.datalocale.fr/fr/dataset/odt_cg_epci2014
 # @format: Shapefile
-gironde-%-shp.zip: ${contours-gironde}
+gironde-%-shp.zip: tmp ${contours-gironde}
 	@printf "Fetching...\n\tGironde GIS data\n"
-	@curl --output gironde-odt_epci2014-shp.zip http://catalogue.datalocale.fr//fr/storage/f/2014-04-14T153618/odt_epci2014.zip
-	unzip gironde-odt_epci2014-shp.zip -d ${contours-gironde}
+	@curl --output tmp/gironde-odt_epci2014-shp.zip http://catalogue.datalocale.fr//fr/storage/f/2014-04-14T153618/odt_epci2014.zip
+	unzip tmp/gironde-odt_epci2014-shp.zip -d ${contours-gironde}
 
 # @alias: get-contours-france
 # EPCI de France
 # @source: http://www.data.gouv.fr/fr/dataset/contours-des-epci-2014
 # @format: Shapefile
-france-%-shp.zip: ${contours-france}
+france-%-shp.zip: tmp ${contours-france}
 	@printf "Fetching...\n\tFrance GIS data\n"
-	@curl --output france-epci-100m-shp.zip http://osm13.openstreetmap.fr/~cquest/openfla/export/epci-20140306-100m-shp.zip
-	unzip france-epci-100m-shp.zip -d ${contours-france}
+	@curl --output tmp/france-epci-100m-shp.zip http://osm13.openstreetmap.fr/~cquest/openfla/export/epci-20140306-100m-shp.zip
+	unzip tmp/france-epci-100m-shp.zip -d ${contours-france}
 
+tmp: 
+	mkdir tmp
 
 ${contours-france}: 
 	mkdir ${contours-france}/
