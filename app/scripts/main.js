fe81f1c5639dcbf2efdbbbaaa2123d175fabbef3
Edouard Lopez <dev+git@edouard-lopez.com>
2014-04-15 04:31:45 +0200

    feat(map,d3): add topojson overlay with d3.js
    See:
    * http://bost.ocks.org/mike/leaflet/
    * https://stackoverflow.com/questions/17662530/how-do-i-get-a-topojson-layer-to-show-up-in-leaflet-using-d3#comment35261685_17967508

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,11 @@
+/**
+* Heavily based on Mike Bostocks work: http://bost.ocks.org/mike/leaflet/
+* @param  {[type]} window    [description]
+* @param  {[type]} document  [description]
+* @param  {[type]} L         [description]
+* @param  {[type]} undefined [description]
+* @return {[type]}           [description]
+*/
 (function (window, document, L, undefined) {
 	'use strict';
 
@@ -14,4 +22,40 @@
 		attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
 	}).addTo(map);
 
+	var svg = d3.select(map.getPanes().overlayPane).append('svg'),
+	g = svg.append('g').attr('class', 'leaflet-zoom-hide');
+
+	d3.json('scripts/gironde-epci.topo.json', function (collection) {
+		var bounds = d3.geo.bounds(topojson.feature(collection, collection.objects['gironde-epci.geo']));
+		var path = d3.geo.path().projection(projectPoint);
+
+
+		var feature = g.selectAll('path')
+			.data(topojson.feature(collection, collection.objects['gironde-epci.geo']).features)
+			.enter().append('path');
+
+		map.on('viewreset', reset);
+		reset();
+
+		// Reposition the SVG to cover the features.
+		function reset() {
+			var bottomLeft = projectPoint(bounds[0]),
+				topRight = projectPoint(bounds[1]);
+
+			svg.attr('width', topRight[0] - bottomLeft[0])
+				.attr('height', bottomLeft[1] - topRight[1])
+				.style('margin-left', bottomLeft[0] + 'px')
+				.style('margin-top', topRight[1] + 'px');
+
+			g.attr('transform', 'translate(' + -bottomLeft[0] + ',' + -topRight[1] + ')');
+			
+			feature.attr('d', path);
+		}
+
+		// Use Leaflet to implement a D3 geographic projection.
+		function projectPoint(x) {
+			var point = map.latLngToLayerPoint(new L.LatLng(x[1], x[0]));
+			return [point.x, point.y];
+		} 
+	});
 }(window, document, L));
