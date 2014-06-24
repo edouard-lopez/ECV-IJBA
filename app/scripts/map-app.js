4e942a5400b8d310204504df995eb0c36c87d0d5
Edouard Lopez <dev+git@edouard-lopez.com>
2014-06-24 17:31:45 +0200

    style(indent): fix indentation

--- app/scripts/map-app.js
+++ app/scripts/map-app.js
@@ -59,232 +59,222 @@ function mapApp() {
 
 	function drawEntities() {
 		return new Promise(function (resolve, reject) {
-		d3.json('scripts/gironde-epci.topo.json', function (dataset) {
-		var geoData = topojson.feature(dataset, dataset.objects['gironde-epci.geo']);
-		var bounds = d3.geo.bounds(geoData);
-		path = d3.geo.path().projection(projectPoint);
+			d3.json('scripts/gironde-epci.topo.json', function (dataset) {
+				var geoData = topojson.feature(dataset, dataset.objects['gironde-epci.geo']);
+				var bounds = d3.geo.bounds(geoData);
+				path = d3.geo.path().projection(projectPoint);
 
-		// addCentre(geoData.features, 'id');
+				// addCentre(geoData.features, 'id');
 
 
-		entity = entities.selectAll('.entity')
-			.data(geoData.features)
-			.enter()
-				.append('path')
-				.attr('class', 'entity')
-			;
-		label = entitiesLabels.selectAll('.entity-label')
-			.data(geoData.features)
-			.enter()
-				.append('text')
-				.attr('class', 'entity-label')
-			;
-
-		// Reposition the SVG to cover the features.
-		function reset() {
-			var bottomLeft = projectPoint(bounds[0]),
-				topRight = projectPoint(bounds[1]);
-
-			svg.attr('width', topRight[0] - bottomLeft[0])
-				.attr('height', bottomLeft[1] - topRight[1])
-				.style('margin-left', bottomLeft[0] + 'px')
-				.style('margin-top', topRight[1] + 'px');
-
-			g.attr('transform', 'translate(' + -bottomLeft[0] + ',' + -topRight[1] + ')');
-
-			entity.attr('d', path)
-				.attr('class', function (d) {
-					return 'entity ' + getCentres(idify(d.id)).join(' ');
-				})
-				.on('mouseover', function (d) {
-					var eid = idify(d.id);
-					highlightEntity(eid);
-					showEntityLabel(eid);
-					showEntityRoute(eid);
-					// reset style on others elements
-					// d3.selectAll('.entity').classed('active', false);
-					d3.selectAll('circle')
-						.classed('highlight', false)
-						.attr('r', props.circle.default)
+				entity = entities.selectAll('.entity')
+					.data(geoData.features)
+					.enter()
+						.append('path')
+						.attr('class', 'entity')
 					;
-					// apply style to element(s)
-					// d3.select(this).classed('active', true);
-					d3.selectAll('circle.' + idify(d.id))
-						.classed('highlight', true)
-						.attr('r', props.circle.active)
+				label = entitiesLabels.selectAll('.entity-label')
+					.data(geoData.features)
+					.enter()
+						.append('text')
+						.attr('class', 'entity-label')
 					;
-					updateCounter(d.id);
-				})
-			;
 
-			label.attr('id', function (d) { return idify(d.id); })
-				.attr('class', function (d) { return 'entity-label ' + idify(d.id); })
-				.attr('transform', function (d) {
-					return 'translate(' + path.centroid(d) + ')';
-				})
-				.attr('x', -20)
-				.attr('dy', '.35em')
-				.text(function (d) { return toProperCase(d.id); })
-			;
-		}
+				// Reposition the SVG to cover the features.
+				function reset() {
+					var bottomLeft = projectPoint(bounds[0]),
+						topRight = projectPoint(bounds[1]);
 
-		map.on('viewreset', reset);
-		reset();
-		resolve();
+					svg.attr('width', topRight[0] - bottomLeft[0])
+						.attr('height', bottomLeft[1] - topRight[1])
+						.style('margin-left', bottomLeft[0] + 'px')
+						.style('margin-top', topRight[1] + 'px');
+
+					g.attr('transform', 'translate(' + -bottomLeft[0] + ',' + -topRight[1] + ')');
+
+					entity.attr('d', path)
+						.attr('class', function (d) {
+							return 'entity ' + getCentres(idify(d.id)).join(' ');
+						})
+						.on('mouseover', function (d) {
+							var eid = idify(d.id);
+							highlightEntity(eid);
+							showEntityLabel(eid);
+							showEntityRoute(eid);
+							// reset style on others elements
+							// d3.selectAll('.entity').classed('active', false);
+							d3.selectAll('circle')
+								.classed('highlight', false)
+								.attr('r', props.circle.default)
+							;
+							// apply style to element(s)
+							// d3.select(this).classed('active', true);
+							d3.selectAll('circle.' + idify(d.id))
+								.classed('highlight', true)
+								.attr('r', props.circle.active)
+							;
+							updateCounter(d.id);
+						})
+					;
+
+					label.attr('id', function (d) { return idify(d.id); })
+						.attr('class', function (d) { return 'entity-label ' + idify(d.id); })
+						.attr('transform', function (d) {
+							return 'translate(' + path.centroid(d) + ')';
+						})
+						.attr('x', -20)
+						.attr('dy', '.35em')
+						.text(function (d) { return toProperCase(d.id); })
+					;
+				}
+
+				map.on('viewreset', reset);
+				reset();
+				resolve();
+			});
 		});
-	});
 	}
 
 	function drawTransport() {
 		d3.csv('scripts/routes-dechets.csv', function (error, dataset) {
-		centre = centres.selectAll('.centre')
-			.data(dataset)
-			.enter()
-			.append('g')
-		;
+			centre = centres.selectAll('.centre')
+				.data(dataset)
+				.enter()
+				.append('g')
+			;
 
-		centre
-			.on('mouseover', function (d) {
-				d3.select(this).attr('r', props.circle.active);
-				// reset style on others elements
-				d3.selectAll('.route').classed('show', false);
-				// apply style to element(s)
-				d3.selectAll('.route.' + idify(d.depart)).classed('show', true);
-			})
-			.on('mouseout', function () {
-				d3.selectAll('.route').classed('show', false);
-			});
-
-		var centrePlace = centre.append('circle');
-		var centreLabel = centre.append('text');
-
-		// Standard enter / update
-		var routePath = routes.selectAll('.route')
-			.data(dataset)
-			.enter()
-			.append('path')
-		;
-
-		var emissionGroup = routes.append('g').attr('id', 'emission');
-		var emission = emissionGroup.selectAll('.emission')
-			.data(dataset)
-			.enter()
-			.append('text')
-		;
-		var qteGroup = routes.append('g').attr('id', 'qte');
-		var qte = qteGroup.selectAll('.qte')
-			.data(dataset)
-			.enter()
-			.append('text')
-		;
-		var distGroup = routes.append('g').attr('id', 'dist');
-		var dist = distGroup.selectAll('.dist')
-			.data(dataset)
-			.enter()
-			.append('text')
-		;
-
-		function attach(d) {
-			var coordDepart = [ d.lon_depart, d.lat_depart ];
-			var coordArrivee = [ d.lon_arrivee, d.lat_arrivee ];
-			return 'translate(' + path.centroid({type: 'LineString', coordinates: [coordDepart, coordArrivee ] }) + ')';
-		}
-
-		function reset() {
 			centre
-				.attr('class', function (d) {
-					return	' centre ' + idify(d.depart) +
-							' from-' + typeCentre[d.niv_depart] +
-							' to-' + typeCentre[d.niv_arrivee];
+				.on('mouseover', function (d) {
+					d3.select(this).attr('r', props.circle.active);
+					// reset style on others elements
+					d3.selectAll('.route').classed('show', false);
+					// apply style to element(s)
+					d3.selectAll('.route.' + idify(d.depart)).classed('show', true);
 				})
-			;
-			centrePlace
-					.attr('r', props.circle.default)
-					.attr('cx', function (d) { return projectDepart(d)[0]; })
-					.attr('cy', function (d) { return projectDepart(d)[1]; })
-					.attr('title', function (d) { return idify(d.depart); })
-			;
-			centreLabel
-				.attr('class', 'label')
-				.attr('transform', function (d) {
-					return 'translate(' +  projectDepart(d)[0] + ', ' + projectDepart(d)[1] + ')';
-				})
-				.text(function (d) { return idify(d.depart); })
+				.on('mouseout', function () {
+					d3.selectAll('.route').classed('show', false);
+				});
+
+			var centrePlace = centre.append('circle');
+			var centreLabel = centre.append('text');
+
+			// Standard enter / update
+			var routePath = routes.selectAll('.route')
+				.data(dataset)
+				.enter()
+				.append('path')
 			;
 
-			routePath.attr('d', function (d) {
-					var coordDepart = [ d.lon_depart, d.lat_depart ];
-					var coordArrivee = [ d.lon_arrivee, d.lat_arrivee ];
-					var vertex = path.centroid({type: 'LineString', coordinates: [coordDepart, coordArrivee ] });
-					return path({
-						type: 'LineString',
-						coordinates: [
-							coordDepart,
-							pointToProjection(vertex),
-							coordArrivee
-						]
-					});
-				})
-				.attr('class', function (d) {
-					return [
-						getEpci(idify(d.depart)), getEpci(idify(d.arrivee)),
-						'route', idify(d.depart), idify(d.arrivee),
-						'from-' + typeCentre[d.niv_depart],
-						'to-' + typeCentre[d.niv_arrivee],
-					].join(' ');
-				})
+			var emissionGroup = routes.append('g').attr('id', 'emission');
+			var emission = emissionGroup.selectAll('.emission')
+				.data(dataset)
+				.enter()
+				.append('text')
 			;
-			routePath
-				// use line between centers
-				// .attr('class', function (d) {
-				// 	return [
-				// 		getEpci(idify(d.depart)), getEpci(idify(d.arrivee)),
-				// 		'arc route', idify(d.depart), idify(d.arrivee),
-				// 		'from-' + typeCentre[d.niv_depart],
-				// 		'to-' + typeCentre[d.niv_arrivee],
-				// 	].join(' ');
-				// })
-
-				// use curves between centers
-				.attr('d', function (d) {
-					var source = projectDepart(d);
-					var target = projectArrivee(d);
-
-					var  dx = target[0] - source[0]
-						,dy = target[1] - source[1]
-						,dr = Math.sqrt(dx * dx + dy * dy)
-						,inOut = Math.round(Math.random(1))
-					;
-					var arc = 'M ' + source[0] + ', ' + source[1]
-							+ 'A ' + dr + ', ' + dr + ',0 0 '+inOut+'  ' + target[0] + ', ' + target[1];
-
-					return arc;
-				})
+			var qteGroup = routes.append('g').attr('id', 'qte');
+			var qte = qteGroup.selectAll('.qte')
+				.data(dataset)
+				.enter()
+				.append('text')
+			;
+			var distGroup = routes.append('g').attr('id', 'dist');
+			var dist = distGroup.selectAll('.dist')
+				.data(dataset)
+				.enter()
+				.append('text')
 			;
 
-			emission.attr('class', function (d) { return ['emission', getEpci(idify(d.depart)), getEpci(idify(d.arrivee))].join(' '); })
-				.attr('y', -5)
-				.attr('dy', '.35em')
-				.attr('transform', function (d) {return attach(d); })
-				.text(function (d) { return Math.round(d.co2, 0) + ' kg'; })
-			;
-			qte.attr('class', function (d) { return ['qte', getEpci(idify(d.depart)), getEpci(idify(d.arrivee))].join(' '); })
-				.attr('y', 5)
-				.attr('dy', '.35em')
-				.attr('transform', function (d) {return attach(d); })
-				.text(function (d) { return Math.round(d.qte, 0) + ' t'; })
-			;
-			dist.attr('class', function (d) { return ['dist', getEpci(idify(d.depart)), getEpci(idify(d.arrivee))].join(' '); })
-				.attr('y', 15)
-				.attr('dy', '.35em')
-				.attr('transform', function (d) {return attach(d); })
-				.text(function (d) { return Math.round(d.dist, 0) + ' km'; })
-			;
-		}
+			function attach(d) {
+				var coordDepart = [ d.lon_depart, d.lat_depart ];
+				var coordArrivee = [ d.lon_arrivee, d.lat_arrivee ];
+				return 'translate(' + path.centroid({type: 'LineString', coordinates: [coordDepart, coordArrivee ] }) + ')';
+			}
 
-		map.on('viewreset', reset);
-		reset();
-	});
+			function reset() {
+				centre
+					.attr('class', function (d) {
+						return	' centre ' + idify(d.depart) +
+								' from-' + typeCentre[d.niv_depart] +
+								' to-' + typeCentre[d.niv_arrivee];
+					})
+				;
+				centrePlace
+						.attr('r', props.circle.default)
+						.attr('cx', function (d) { return projectDepart(d)[0]; })
+						.attr('cy', function (d) { return projectDepart(d)[1]; })
+						.attr('title', function (d) { return idify(d.depart); })
+				;
+				centreLabel
+					.attr('class', 'label')
+					.attr('transform', function (d) {
+						return 'translate(' +  projectDepart(d)[0] + ', ' + projectDepart(d)[1] + ')';
+					})
+					.text(function (d) { return idify(d.depart); })
+				;
+
+				routePath.attr('d', function (d) {
+						var coordDepart = [ d.lon_depart, d.lat_depart ];
+						var coordArrivee = [ d.lon_arrivee, d.lat_arrivee ];
+						var vertex = path.centroid({type: 'LineString', coordinates: [coordDepart, coordArrivee ] });
+						return path({
+							type: 'LineString',
+							coordinates: [
+								coordDepart,
+								pointToProjection(vertex),
+								coordArrivee
+							]
+						});
+					})
+					.attr('class', function (d) {
+						return [
+							getEpci(idify(d.depart)), getEpci(idify(d.arrivee)),
+							'route', idify(d.depart), idify(d.arrivee),
+							'from-' + typeCentre[d.niv_depart],
+							'to-' + typeCentre[d.niv_arrivee],
+						].join(' ');
+					})
+				;
+				routePath
+
+					.attr('d', function (d) {
+						var source = projectDepart(d);
+						var target = projectArrivee(d);
+
+						var  dx = target[0] - source[0]
+							,dy = target[1] - source[1]
+							,dr = Math.sqrt(dx * dx + dy * dy)
+							,inOut = Math.round(Math.random(1))
+						;
+						var arc = 'M ' + source[0] + ', ' + source[1]
+								+ 'A ' + dr + ', ' + dr + ',0 0 '+inOut+'  ' + target[0] + ', ' + target[1];
+
+						return arc;
+					})
+				;
+
+				emission.attr('class', function (d) { return ['emission', getEpci(idify(d.depart)), getEpci(idify(d.arrivee))].join(' '); })
+					.attr('y', -5)
+					.attr('dy', '.35em')
+					.attr('transform', function (d) {return attach(d); })
+					.text(function (d) { return Math.round(d.co2, 0) + ' kg'; })
+				;
+				qte.attr('class', function (d) { return ['qte', getEpci(idify(d.depart)), getEpci(idify(d.arrivee))].join(' '); })
+					.attr('y', 5)
+					.attr('dy', '.35em')
+					.attr('transform', function (d) {return attach(d); })
+					.text(function (d) { return Math.round(d.qte, 0) + ' t'; })
+				;
+				dist.attr('class', function (d) { return ['dist', getEpci(idify(d.depart)), getEpci(idify(d.arrivee))].join(' '); })
+					.attr('y', 15)
+					.attr('dy', '.35em')
+					.attr('transform', function (d) {return attach(d); })
+					.text(function (d) { return Math.round(d.dist, 0) + ' km'; })
+				;
+			}
+
+			map.on('viewreset', reset);
+			reset();
+		});
 	}
 
 	/**
